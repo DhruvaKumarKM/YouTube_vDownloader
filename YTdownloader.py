cbf5fa765e178d27684b7b2b69bd2f9b30698e8e
Dhruva <dhruva5000@gmail.com>
2023-02-11 22:39:01 +0530

    YouTube Downloader

---
+++ YTdownloader.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from pytube import YouTube
+
+def download_video():
+    url = url_entry.get()
+    path = filedialog.askdirectory()
+    try:
+        yt = YouTube(url)
+        video = yt.streams.first()
+        video.download(path)
+        messagebox.showinfo("Info", "Video downloaded successfully!")
+    except:
+        messagebox.showerror("Error", "An error occurred while downloading the video.")
+
+root = tk.Tk()
+root.title("YouTube Video Downloader created by DHRUVA")
+root.config(bg='grey')
+root.geometry("600x400")
+root.geometry('500x570+100+30')
+
+url_label = tk.Label(root, text="Enter the URL of the YouTube video:")
+url_label.pack(pady=10)
+
+url_entry = tk.Entry(root)
+url_entry.pack(pady=10,padx=20)
+
+download_button = tk.Button(root, text="Download", command=download_video)
+download_button.pack(pady=5)
+
+root.mainloop()
